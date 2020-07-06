b5b859065401e15a48863c2f0ecc90288b2f641d
Troy Dawson <troyd9216@gmail.com>
2020-07-06 23:42:39 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,10 +23,10 @@ def output_array(array)
 end 
 
 def return_string_array(array)
-  count = 0 
-  while count < array.length do 
-    array[count].to_s 
-    count += 1 
+  count = 0
+  while count < array.length do
+    array[count] = array[count].to_s
+    count += 1
   end
-  new_array
-end 
+  array
+end
